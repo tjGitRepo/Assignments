60641de19b3e557a79901ac9c6e0d42b5b3b7ceb
AdapaTeja <asvkteja@gmail.com>
2025-05-23 20:13:36 +0530

    Assigment1_Datatypes

---
+++ src/assignmments/Assignment1_DataTypes.java
@@ -0,0 +1,115 @@
+package assignmments;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		
+		//1. Printing float using 'System.out.printf'
+			float Temp = 25.5f ;
+			System.out.printf("1. Temperature of a city in degrees Celsius: %.1f%n",Temp);
+			
+		
+		//2. Printing boolean using 'System.out.format'
+			boolean Order = true ;
+			System.out.format("2. Whether a customer has placed an order: %b%n",Order);
+			
+			
+		//3. Printing boolean using 'System.out.println'
+			String Phn = "123-456-7890" ;
+			System.out.printf("3. Person's phone number: %s",Phn);
+			
+		//4. Printing float using 'System.out.printf'
+			float Amt = 1000.50f ;
+			System.out.printf("4. Amount of money in a customer's bank account: %.2f%n",Amt);
+			
+		//5. Printing String using 'System.out.printf'
+			String email = "john.doe@example.com" ;
+			System.out.printf("5. Person's email address: %s%n",email);		
+		
+		//6 Printing multiple decimal values with float,double using 'System.out.printf'
+			float latitude = 37.7749f;
+			double longitude = -122.4194527122;
+			
+			System.out.format("6. Coordinates of a location (latitude, longitude): %f, %.10f%n",latitude,longitude );
+			
+		//7 Printing boolean using 'System.out.printf'
+			boolean status = false;
+			
+			System.out.printf("7. Marital status: %s%n",status );
+			
+		//8 Printing String using 'System.out.print'
+			String occupation = "Software Engineer";
+			System.out.print("8. Person's occupation: "+ occupation );
+			System.out.println();
+			
+		//9 Printing String using 'System.out.println'
+			String color = "Blue";
+			System.out.println("9. Person's favourite colour: "+ color );
+			
+			
+		//10 Printing short using 'System.out.println'
+			short year = 2025;
+			System.out.println("10.Current year: "+ year );
+			
+		//11 Printing String using 'System.out.println'
+			String followers = "1,000,000";
+			System.out.println("11. Number of followers on a social media platform: "+ followers );
+		
+		//12. Printing float using 'System.out.format'
+			float Rating = 7.5f ;
+			System.out.format("12.Rating of a movie: %.1f%n",Rating);
+			
+		//13. Printing char using 'System.out.format'
+			char blood = 'A';
+			System.out.format("13.Person's blood type: %c",blood);
+			
+		//14 Printing String using 'System.out.println'
+			String book = "\"To Kill a Mockingbird\"";
+			System.out.println("14.Title of a book: "+ book );
+			
+		//15 Printing Integer using 'System.out.println'
+			int emp = 500;
+			System.out.println("15. Number of employees in a company: "+ emp );
+		
+		//16 Printing String using 'System.out.println'
+			String time = "02:30 PM";
+			System.out.println("16. Time of an event: "+ time );
+		
+		//17 Printing String using 'System.out.printf'
+			String country = "United States";
+			System.out.printf("17. Name of a country: %s%n", country );
+		
+		//18 Printing String using 'System.out.println'
+			String eyeColor = "Brown";
+			System.out.println("18.Person's eye color: "+ eyeColor );
+			
+		//19 Printing String using 'System.out.println'
+				String birthPlace = "New York City";
+				System.out.printf("19. Person's birthplace: "+ birthPlace );
+				System.out.println();
+				
+		//20 Printing String using 'System.out.println'
+				float dist = 200.5f;
+				System.out.printf("20. Distance between two cities: "+ dist );		
+		
+		
+		
+			
+			 
+			
+			 	 
+				
+			
+			
+	
+		
+		
+		
+		
+		
+	}
+
+}
